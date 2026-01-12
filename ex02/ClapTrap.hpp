01486ff78d89632429c9b8d522e07f70b702a02a
Zakaria Ajaddou <zajaddou@e3r7p15.1337.ma>
2026-01-12 13:53:11 +0100

    update

--- ex02/ClapTrap.hpp
+++ ex02/ClapTrap.hpp
@@ -6,14 +6,13 @@
 /*   By: zajaddou <zajaddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 14:19:26 by zajaddou          #+#    #+#             */
-/*   Updated: 2026/01/10 14:19:29 by zajaddou         ###   ########.fr       */
+/*   Updated: 2026/01/12 13:05:27 by zajaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAPTRAP_HPP
-# define CLAPTRAP_HPP
+#pragma once
 
-# include <iostream>
+#include <iostream>
 
 class ClapTrap {
     protected:
@@ -26,11 +25,9 @@ class ClapTrap {
         ClapTrap(const std::string &name);
         ClapTrap(const ClapTrap &src);
         ClapTrap &operator=(const ClapTrap &src);
-        ~ClapTrap();
+        virtual ~ClapTrap();
 
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
 };
-
-#endif
